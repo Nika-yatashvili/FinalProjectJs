20e614f00eff7faf3a72200167b3487abf883323
Nika-yatashvili <nika.katashvili2@gmail.com>
2025-04-03 13:34:29 -0700

    add User Authenthication

--- index.js
+++ index.js
@@ -1,12 +1,64 @@
+let isUserLoggedIn = false;
+let username = "";
+const signInForm = document.querySelector('#signInForm');
+const logOut = document.querySelector(".logOut")
+const usernameParagraph = document.querySelector('#userName');
+const newsDiv = document.querySelector("#newsContainer");
+
+function renderEmpty() {
+    const div = document.createElement("div");
+    div.classList.add("emptyArticleContainer");
+    div.innerHTML = `
+        <h4>Please Login to view today's news</h4>
+    `;
+    return div;
+}
+
+function logoutHandler() {
+    logOut.classList.add("hidden")
+    usernameParagraph.innerHTML = ""
+    newsDiv.innerHTML = ""
+    newsDiv.appendChild(renderEmpty())
+}
+
+function loginHandler(userInputValue) {
+    usernameParagraph.innerHTML = userInputValue;
+    signInForm.classList.add("hidden")
+    logOut.classList.remove("hidden")
+    newsDiv.innerHTML = ""
+
+    fetchNews();
+}
+
+if (!isUserLoggedIn) {
+    logoutHandler()
+}
+
+
+logOut.addEventListener("click", (e) => {
+    e.preventDefault()
+    signInForm.classList.remove("hidden")
+    logoutHandler()
+})
+
+signInForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    isUserLoggedIn = true;
+
+    const usernameInput = document.querySelector("#usernameId").value;
+
+    if (usernameInput.trim().length > 0) {
+        loginHandler(usernameInput)
+    }
+})
+
+
 
 const apiKey = 'f1330f15f7d847f995a127c0f8edca11';
 const searchQuery = "tesla";
-const page = 1;
+
+const currentPage = 1;
 const pageSize = 10;
-
-const newsDiv = document.querySelector("#newsContainer");
-
-
 
 function createNewsDiv(news) {
     const div = document.createElement("div");
@@ -20,13 +72,13 @@ function createNewsDiv(news) {
             </p>
         </div>
         <p id="articleDescription">${news.description}</p>
-        ${news.urlToImage ? `<img id="articleImage" src="${news.urlToImage}" alt="article-image">` : ""}
+        ${news.urlToImage ? `<img id="articleImage" src="${news.urlToImage}" alt="article-image">` : `<span></span>`}
     `;
     return div;
 }
 
 function fetchNews() {
-    fetch(`https://newsapi.org/v2/everything?q=${searchQuery}&from=2025-02-28&sortBy=publishedAt&page=${page}&pageSize=${pageSize}&apiKey=${apiKey}`)
+    fetch(`https://newsapi.org/v2/everything?q=${searchQuery}&sortBy=publishedAt&pageSize=${pageSize}&page=${currentPage}&apiKey=${apiKey}`)
         .then(
             (response) => response.json()
         ).then((data) => {
@@ -40,5 +92,3 @@ function renderNews(news) {
         newsDiv.appendChild(createNewsDiv(news));
     });
 }
-
-fetchNews();
